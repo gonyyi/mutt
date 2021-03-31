7d6657dbc2bbdef223b30654763cf65917f2ba56
Gon <29622355+gonyyi@users.noreply.github.com>
2021-03-30 21:20:01 -0500

    Update checkParam.go

--- checkParam.go
+++ checkParam.go
@@ -3,11 +3,11 @@
 
 package mutt
 
-func CheckParamString(fields ...string) error {
+func CheckParamString(fields ...string) bool {
 	for i := 0; i < len(fields); i++ {
 		if fields[i] == "" {
-			return ERR_MISSING_REQUIRED_FIELDS
+			return false
 		}
 	}
-	return nil
+	return true
 }
